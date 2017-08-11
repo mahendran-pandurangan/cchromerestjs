cf190241326e62aa6e0a7d4566503691740a0c7a
Mahendran Pandurangan <mpandura@akamai.com>
2017-08-11 11:39:15 -0700

    fixed iteration of test by moving done()

--- test/testRestWs.js
+++ test/testRestWs.js
@@ -2,17 +2,14 @@ var restCall = require('../lib/backEndUtils');
 var logger = require('../lib/logger');
 xlProvider = require('../lib/excel-provider.js');
 
-//const mochaTimeOut = 90000;
+const mochaTimeOut = 90000;
 var dataProvider;
 describe('rest api suite', function () {
   it('rest api suite', function (done) {
-    //this.timeout(mochaTimeOut);
+    this.timeout(mochaTimeOut);
 
     dataProvider = xlProvider.readExcelData('resources/datasheet/RestURLs.xlsx',
         'MC');
-
-    //this.timeout(mochaTimeOut);
-    // var dataProvider = require('../resources/json-output/MC-data.json');
     logger.info('In rest test');
     dataProvider.forEach(function (readData) {
       if (readData.url !== '' && readData.flag === 'Y') {
@@ -26,9 +23,10 @@ describe('rest api suite', function () {
         catch (err) {
           logger.error('Error due to' + err);
         }
-        done();
+       ;
       }
     });
+    done();
   });
 })
 
